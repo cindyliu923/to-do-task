1422c0075df64016e64b440ab8d2a49c4fb4edfb
Cindy Liu <cindyliu923@gmail.com>
2019-02-02 10:04:30 +0800

    add CRUD action for task
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+  before_action :set_task, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "task was successfully created"
+      redirect_to tasks_url
+    else
+      flash.now[:alert] = "task was failed to create"
+      render :action => :new
+    end
+  end
+
+  def update
+    if @task.update_attributes(task_params)
+      flash[:notice] = "task was successfully updated"
+      redirect_to task_path(@task)
+    else
+      flash.now[:alert] = "task was failed to update"
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url
+    flash[:alert] = "task was deleted"
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
