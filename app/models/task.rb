50fc4072bc0e6c92d8cb81380e61a6b0e262bde3
Cindy Liu <cindyliu923@gmail.com>
2019-02-04 21:48:00 +0800

    setup task model for status
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,21 @@
 class Task < ApplicationRecord
+  include AASM
+
   validates :title, :content, presence: true
+
+  enum status: { todo: 0, doing: 1, done: 2 }
+
+  aasm :column => :status, :enum => true do
+    state :todo, :initial => true
+    state :doing, :done
+
+    event :up do
+      transitions from: :todo, to: :doing
+    end
+
+    event :down do
+      transitions from: :doing, to: :done
+    end
+  end
+
 end
