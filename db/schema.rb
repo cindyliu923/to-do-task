fceca935ea090f1cdf9d83b997dc7e1a41642c69
Cindy Liu <cindyliu923@gmail.com>
2019-02-04 21:03:35 +0800

    add status to tasks db
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_033004) do
+ActiveRecord::Schema.define(version: 2019_02_04_113158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_02_04_033004) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline"
+    t.integer "status", default: 0, null: false
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
