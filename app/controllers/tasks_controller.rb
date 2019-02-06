948e27c00ca918b3619787c96184e52e9fbdae56
Cindy Liu <cindyliu923@gmail.com>
2019-02-06 17:29:42 +0800

    update view and controller for add kaminari
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   def index
     @q = Task.ransack(params[:q])
-    @tasks = @q.result.order(created_at: :desc)
+    @tasks = @q.result.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def new
