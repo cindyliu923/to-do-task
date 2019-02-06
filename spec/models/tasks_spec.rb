db53953e3a850dabf01a3cf462b2f136b06f3d2e
Cindy Liu <cindyliu923@gmail.com>
2019-02-06 15:48:14 +0800

    update task model test for priority
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -3,28 +3,33 @@ require "rails_helper"
 RSpec.describe Task, :type => :model do
   subject { described_class.new }
   it { should define_enum_for(:status).with_values([:todo, :doing, :done]) }
+  it { should define_enum_for(:priority).with_values([:low, :medium, :high]) }
 
   it "is valid with valid attributes" do
     subject.title = "task title"
     subject.content = "task content"
     subject.deadline = 2.days.from_now
+    subject.priority = :low
     expect(subject).to be_valid
   end
 
   it "is not valid without title and content" do
     subject.deadline = 2.days.from_now
+    subject.priority = :low
     expect(subject).to_not be_valid
   end
 
   it "is not valid without a title" do
     subject.content = "task content"
     subject.deadline = 2.days.from_now
+    subject.priority = :low
     expect(subject).to_not be_valid
   end
 
   it "is not valid without a content" do
     subject.title = "task title"
     subject.deadline = 2.days.from_now
+    subject.priority = :low
     expect(subject).to_not be_valid
   end
 
