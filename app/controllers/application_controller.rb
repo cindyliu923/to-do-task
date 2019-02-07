c7cd44c0fcd863415169ad7c513f084994bff14a
Cindy Liu <cindyliu923@gmail.com>
2019-02-07 22:51:05 +0800

    setup user authorize and current user
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :set_locale
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 
   def set_locale
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
 
   def new
-    @q = Task.ransack(params[:q])
     @user = User.new
   end
 
   def create
-    @q = Task.ransack(params[:q])
     @user = User.create(user_params)
     if @user.save
       redirect_to root_path
