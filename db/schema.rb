ffc5472aeb0e7f50fb9a0647b5286e1362f72055
Cindy Liu <cindyliu923@gmail.com>
2019-02-07 15:31:29 +0800

    add user model
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_050850) do
+ActiveRecord::Schema.define(version: 2019_02_07_070553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2019_02_06_050850) do
     t.integer "priority", default: 0, null: false
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
