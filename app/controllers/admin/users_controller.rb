e69596f5d3ccb25081753bb75f81ffed203c950c
Cindy Liu <cindyliu923@gmail.com>
2019-02-08 16:59:43 +0800

    add CRUD for admin user
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,55 @@
 class Admin::UsersController < ApplicationController
+  before_action :authorize
+  before_action :set_user, :only => [:show, :edit, :update, :destroy]
+  before_action :search_task
 
   def index
-    @q = current_user.tasks.ransack(params[:q])
     @users = User.page(params[:page]).per(10)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = I18n.t("users.notice.create")
+      redirect_to admin_users_url
+    else
+      flash.now[:alert] = I18n.t("users.alert.create")
+      render :action => :new
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:notice] = I18n.t("users.notice.update")
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:alert] = I18n.t("users.alert.update")
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url
+    flash[:alert] = I18n.t("users.alert.destroy")
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def search_task
+    @q = current_user.tasks.ransack(params[:q])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
