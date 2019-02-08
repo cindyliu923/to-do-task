d8fc7cb168498fc90239212861611608b6f73d38
Cindy Liu <cindyliu923@gmail.com>
2019-02-08 17:22:57 +0800

    add task dependent destroy for delete user
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
 
   validates :name, :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }
