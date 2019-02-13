6539000494a46dee1d90f48ccd2ee1fb21845fa2
Cindy Liu <cindyliu923@gmail.com>
2019-02-13 08:52:00 +0800

    update button style
    Signed-off-by: Cindy Liu <cindyliu923@gmail.com>

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   def pick_task_status(task)
     if task.todo?
-      link_to t("tasks.up"), up_task_path(task), method: :patch, class: "btn btn-success"
+      link_to t("tasks.up"), up_task_path(task), method: :patch, class: "btn btn-info"
     elsif task.doing?
       link_to t("tasks.down"), down_task_path(task), method: :patch, class: "btn btn-success"
     else
